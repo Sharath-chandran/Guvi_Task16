1fe6138a564a4c2daae68fc502100a6425cfc623
Sharath-chandran <sharathdfire@gmail.com>
2025-07-20 01:13:35 -0700

    Guvi TRask 16 - 3rd commit

--- page/world_population_pom.py
+++ page/world_population_pom.py
@@ -1,14 +1,19 @@
-# world_population
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# POM class named PopulationPage.
+# It encapsulates the logic to retrieve the population count from a web page
 class PopulationPage:
 
+    # Initializes the PopulationPage class with a WebDriver instance.
+    # And defines a locator for the population count element using XPath.
     def __init__(self, driver):
         self.driver = driver
         self.population_count = (By.XPATH, "//*[@id='content']/div/div/app-page/header/app-asset-title/section/div/app-counter/div/div[1]")
 
+    # Method to Get Population Count
     def total_population_count(self):
         WebDriverWait(self.driver, timeout=10).until(EC.visibility_of_element_located(self.population_count))
+        # * unpacks the tuple (By.XPATH, "locator") into find_element(By.XPATH, "locator")
         return self.driver.find_element(*self.population_count).text
--- test/test_population_count.py
+++ test/test_population_count.py
@@ -2,14 +2,23 @@ import time
 from page.world_population_pom import PopulationPage
 
 def test_live_count (driver):
+        # Creates an instance of the PopulationPage
         page = PopulationPage(driver)
         page.total_population_count()
         time.sleep(10)
+
+        # Continuous Monitoring Loop
         print("Press CTRL+C to stop monitoring population count...")
+
+        #  infinite loop to continuously fetch the live population count
         try:
             while True:
                 count = page.total_population_count()
+                # updates the console in-place — simulates a live feed effect.
                 print(f"Current Population Count: {count}", end="\r")
                 time.sleep(1)  # Update every second
+
+        # Exit with KeyboardInterrupt
+        # loop keeps running until you manually stop it by pressing CTRL+C.
         except KeyboardInterrupt:
                 print("\nMonitoring stopped by user.")
